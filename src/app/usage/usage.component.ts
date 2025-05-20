f2ec56e5f19a49bedf03054cf576aaf8511a6350
suganya.p <suganya.p@ENSDESK-04>
2025-05-20 17:38:23 +0530

    createdashboardandusagepage

--- src/app/usage/usage.component.ts
+++ src/app/usage/usage.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-usage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './usage.component.html',
   styleUrl: './usage.component.css'
 })
 export class UsageComponent {
-
+//  
+ usageData = [
+    { date: '2025-05-01', requests: 120, users: 15, storage: '2.5GB' },
+    { date: '2025-05-02', requests: 145, users: 18, storage: '2.6GB' },
+    // Add more data as needed
+  ];
 }
